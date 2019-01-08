86993470cb3cc38c4a241776afc02104950953f3
Charles Jacquin <charles@cj-code.me>
2019-01-08 01:12:14 +0100

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Type definitions for peertube-client 0.0.0
+ * Project: https://github.com/charjac/peertube-client
+ * Definitions by: Charles Jacquin <https://github.com/charjac>
+ * Definitions: https://github.com/charjac/peertube-client
+ * TypeScript Version: 3.2
+ */
+
+export { Greeter } from './greeter'
